ce57d5c7f908717a32d786a7e0a737200013aee0
Евгений С <evgeny.surovegin@gmail.com>
2025-11-05 14:14:42 +0300

    Lab4

--- Source1.cpp
+++ Source1.cpp
@@ -9,6 +9,16 @@ template<typename T> class TSinglyList
 			value = data;
 			pNext = n;
 		}
+		TNode& operator=(const TNode& node)
+		{
+			value = node.value;
+			pNext = node.pNext;
+		}
+		//void swap(TNode& 1hs, TNode& 2hs)
+		//{
+		//	std::swap(1hs.value, 2hs.value);
+		//	std::swap(1hs.pNext, 2hs.pNext);
+		//}
 	};
 	TNode* pFirst;
 	size_t sz;
@@ -18,16 +28,45 @@ public:
 		pFirst = nullptr;
 		sz = 0;
 	}
-	TSinglyList(const vector<T>& v);
-	TSinglyList(const TSinglyList& list);
-	TSinglyList(TSinglyList&& list) noexcept;
-	TSinglyList& operator=(const TSinglyList& list);
-	TSinglyList& operator=(const TSinglyList&& list)noexcept;
+	TSinglyList(const vector<T>& v)
+	{
+
+	}
+	TSinglyList(const TSinglyList& list)
+	{
+		sz = list.sz;
+		pFirst = list.pFirst;
+	}
+	TSinglyList(TSinglyList&& list) noexcept
+	{
+		pFirst = nullptr;
+		swap(*this, list);
+	}
+	TSinglyList& operator=(const TSinglyList& list)
+	{
+		pFirst = list.pFirst;
+		sz = list.sz;
+		return *this;
+	}
+	void swap(TSinglyList& 1hs, TSinglyList& 2hs)
+	{
+		std::swap(1hs.sz, 2hs.sz);
+		std::swap(1hs.pFirst, 2hs.pFirst);
+	}
+	TSinglyList& operator=(const TSinglyList&& list)noexcept
+	{
+		swap(*this, list);
+		return *this;
+	}
 	~TSinglyList();
 	size_t size() const noexcept;
 	bool IsEmpty() const noexcept;
 	T& Front()noexcept;
-	void PushFront(const T& val);
+	void PushFront(const T& val)
+	{
+		TNode* t = new TNode(val, pFirst);
+		pFirst = t;
+	}
 	void PopFront()noexcept;
 	T& operator[](size_t pos);
 	void PushAfter(size_t pos, const T& val);
